067eef5d5d069513fa98ff7de335a2d627bf9c25
Marcus Gotling <marcus@gotling.se>
2014-11-30 00:22:48 +0100

    Comment and PEP8

--- random-music.py
+++ random-music.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""Random Music by Marcus Götling, www.gotling.se
+"""Random Music by Marcus Götling, marcus.gotling.se
 
 Usage:
     random-music.py <src> <dst> <size> [--delete]
@@ -15,8 +15,10 @@ import random
 import shutil
 from docopt import docopt
 
+
 def regular_file(item):
     return not item.startswith('.')
+
 
 def get_folders(directory):
     f = []
@@ -27,10 +29,12 @@ def get_folders(directory):
         break
     return d
 
+
 def remove_and_create_folder(directory):
     shutil.rmtree(directory)
     if not os.path.exists(directory):
         os.makedirs(directory)
+
 
 def get_size(directory):
     total_size = 0
@@ -40,30 +44,35 @@ def get_size(directory):
             total_size += os.path.getsize(fp)
     return total_size
 
+
 def is_top_dir(directory):
     return len(get_folders(directory)) == 0
 
+
 def random_walk(source, destination):
-    if(is_top_dir(source)):
+    if is_top_dir(source):
         copy_folder(source, destination)
     else:
         random_walk(os.path.join(source, random.choice(get_folders(source))), destination)
+
 
 def copy_folder(source, destination):
     if not os.path.exists(os.path.join(destination, os.path.basename(source))):
         print os.path.basename(source)
         try:
-        	shutil.copytree(source, os.path.join(destination, os.path.basename(source)))
+            shutil.copytree(source, os.path.join(destination, os.path.basename(source)))
         except Exception as e:
-        	print "Could not copy folder '%s'" % source
+            print "Could not copy folder '%s'" % source
+
 
 def space_left(directory, max_size):
     return get_size(directory) < max_size * 1024 * 1024
 
+
 def main():
     arguments = docopt(__doc__, version='Random Music 0.1')
 
-    if (arguments["--delete"]):
+    if arguments["--delete"]:
         remove_and_create_folder(arguments["<dst>"])
 
     while space_left(arguments["<dst>"], arguments["<size>"]):
